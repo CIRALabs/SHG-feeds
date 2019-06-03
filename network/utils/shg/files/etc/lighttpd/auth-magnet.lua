04cd3e45b4e86dbd570b68adb96db57b16cd910b
Audric Schiltknecht <audric.schiltknecht@viagenie.ca>
2019-06-03 17:31:04 -0400

    shg: Add auth-magnet script to validate JWT
    - Deploy script in lighttpd config directory.
    - Add dep on lua-basexx and luacrypto.

---
+++ network/utils/shg/files/etc/lighttpd/auth-magnet.lua
@@ -0,0 +1,185 @@
+-- Taken from https://github.com/x25/luajwt
+-- The MIT License (MIT)
+-- 
+-- Copyright (c) 2014
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+local cjson  = require 'cjson'
+local basexx = require 'basexx'
+local crypto = require 'crypto'
+
+local alg_sign = {
+	['HS256'] = function(data, key) return crypto.hmac.digest('sha256', data, key, true) end,
+	['HS384'] = function(data, key) return crypto.hmac.digest('sha384', data, key, true) end,
+	['HS512'] = function(data, key) return crypto.hmac.digest('sha512', data, key, true) end,
+}
+
+local alg_verify = {
+	['HS256'] = function(data, signature, key) return signature == alg_sign['HS256'](data, key) end,
+	['HS384'] = function(data, signature, key) return signature == alg_sign['HS384'](data, key) end,
+	['HS512'] = function(data, signature, key) return signature == alg_sign['HS512'](data, key) end,
+}
+
+local function b64_encode(input)	
+	local result = basexx.to_base64(input)
+	
+	result = result:gsub('+','-'):gsub('/','_'):gsub('=','')
+
+	return result
+end
+
+local function b64_decode(input)
+--	input = input:gsub('\n', ''):gsub(' ', '')
+
+	local reminder = #input % 4
+
+	if reminder > 0 then
+		local padlen = 4 - reminder
+		input = input .. string.rep('=', padlen)
+	end
+
+	input = input:gsub('-','+'):gsub('_','/')
+
+	return basexx.from_base64(input)
+end
+
+local function tokenize(str, div, len)
+	local result, pos = {}, 0
+
+	for st, sp in function() return str:find(div, pos, true) end do
+
+		result[#result + 1] = str:sub(pos, st-1)
+		pos = sp + 1
+
+		len = len - 1
+
+		if len <= 1 then
+			break
+		end
+	end
+
+	result[#result + 1] = str:sub(pos)
+
+	return result
+end
+
+local function encode(data, key, alg)
+	if type(data) ~= 'table' then return nil, "Argument #1 must be table" end
+	if type(key) ~= 'string' then return nil, "Argument #2 must be string" end
+
+	alg = alg or "HS256" 
+
+	if not alg_sign[alg] then
+		return nil, "Algorithm not supported"
+	end
+
+	local header = { typ='JWT', alg=alg }
+
+	local segments = {
+		b64_encode(cjson.encode(header)),
+		b64_encode(cjson.encode(data))
+	}
+
+	local signing_input = table.concat(segments, ".")
+
+	local signature = alg_sign[alg](signing_input, key)
+
+	segments[#segments+1] = b64_encode(signature)
+
+	return table.concat(segments, ".")
+end
+
+local function decode(data, key, verify)
+	if key and verify == nil then verify = true end
+	if type(data) ~= 'string' then return nil, "Argument #1 must be string" end
+	if verify and type(key) ~= 'string' then return nil, "Argument #2 must be string" end
+
+	local token = tokenize(data, '.', 3)
+
+	if #token ~= 3 then
+		return nil, "Invalid token"
+	end
+
+	local headerb64, bodyb64, sigb64 = token[1], token[2], token[3]
+
+	local ok, header, body, sig = pcall(function ()
+
+		return	cjson.decode(b64_decode(headerb64)), 
+			cjson.decode(b64_decode(bodyb64)),
+			b64_decode(sigb64)
+	end)	
+
+	if not ok then
+		return nil, "Invalid json"
+	end
+
+	if verify then
+
+		if not header.typ or header.typ ~= "JWT" then
+			return nil, "Invalid typ"
+		end
+
+		if not header.alg or type(header.alg) ~= "string" then
+			return nil, "Invalid alg"
+		end
+
+		if body.exp and type(body.exp) ~= "number" then
+			return nil, "exp must be number"
+		end
+
+		if body.nbf and type(body.nbf) ~= "number" then
+			return nil, "nbf must be number"
+		end
+
+		if not alg_verify[header.alg] then
+			return nil, "Algorithm not supported"
+		end
+
+		if not alg_verify[header.alg](headerb64 .. "." .. bodyb64, sig, key) then
+			return nil, "Invalid signature"
+		end
+
+		if body.exp and os.time() >= body.exp then
+			return nil, "Not acceptable by exp"
+		end
+
+		if body.nbf and os.time() < body.nbf then
+			return nil, "Not acceptable by nbf"
+		end
+	end
+
+	return body
+end
+
+-- Our code starts here
+print("Requesting access to restricted uri\t" .. lighty.env["request.uri"])
+local token = lighty.request["Authorization"]
+if (token == nil) then
+	print("Nil token, denied")
+	return 401
+end
+-- token: Bearer\sJWT
+token, _ = string.gsub(token, "Bearer%s+", "")
+-- token: JWT
+decoded, err = decode(token, "1234", true)
+
+if (err ~= nil) then
+	print("Error: " .. err)
+	return 401
+end
