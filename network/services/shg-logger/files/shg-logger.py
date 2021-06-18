305ad1e12c2407d2b02fdcd68d4db30654317f43
Jason Blakey <jblakey@gmail.com>
2021-06-18 11:09:21 -0400

    Fixed typo

--- network/services/shg-logger/files/shg-logger.py
+++ network/services/shg-logger/files/shg-logger.py
@@ -17,7 +17,7 @@ data collection server.
 """
 
 # Required constants
-MQTT_TOPICS = ['SPIN/traffic', 'SHG/REJECT']
+MQTT_TOPICS = ['SPIN/traffic', 'SSHG/REJECT']
 MQTT_PORT = 1883
 MQTT_HOST = '127.0.0.1'
 CERT = '/etc/shg/certificates/jrc_prime256v1.crt'
@@ -211,7 +211,7 @@ def on_message(_client, userdata, message):
     # Clean up the json, SPIN inserts some garbage values...
     if 'result' in json_payload.keys():
         # Add a note so that we can locate SPIN traffic later...
-        json_payload['result']['log_type'] = message.topic
+        json_payload['result']['mqtt_topic'] = message.topic
 
         json_payload['result'].pop('total_size', None)
         json_payload['result'].pop('total_count', None)
